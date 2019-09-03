75bc35dfd774bba26042329b16e0fdcd67022984
saltys <protik.sarkar@appliedtechnology.se>
2019-09-03 21:43:16 +0200

    Updates

---
+++ precache-manifest.0d0b7b1e9aac382b21308a14088a3296.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea876c46e4b4a5c2995e9788e55268a",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/index.html"
+  },
+  {
+    "revision": "c655fd2a728d6cd1d67c",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/css/2.0bfc02ec.chunk.css"
+  },
+  {
+    "revision": "c458cb34e5e6a3b9f3fd",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/css/main.cc551e77.chunk.css"
+  },
+  {
+    "revision": "c655fd2a728d6cd1d67c",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/js/2.47a2bdd5.chunk.js"
+  },
+  {
+    "revision": "c458cb34e5e6a3b9f3fd",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/js/main.64af65fc.chunk.js"
+  },
+  {
+    "revision": "4a5e3cd4e680792909ab",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/js/runtime~main.74c4d4a1.js"
+  },
+  {
+    "revision": "4124e33cdc3113a754499da48420ef59",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/hx2.4124e33c.png"
+  },
+  {
+    "revision": "866c7a7d32e6e4ad40d230a60fd7419d",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/hx3.866c7a7d.png"
+  },
+  {
+    "revision": "0b26a7f3d8e7368c613b169669158293",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/hx4.0b26a7f3.png"
+  },
+  {
+    "revision": "24a6657fc723ea99c4b2d00542dfe8e5",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/hx5.24a6657f.png"
+  },
+  {
+    "revision": "17e3f84ec1a8f951c1c8eb19b45d15bc",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/profile.17e3f84e.jpg"
+  },
+  {
+    "revision": "9d7f39c8301f91334e0158824766a296",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/wix1.9d7f39c8.png"
+  },
+  {
+    "revision": "2088c8d6e053a9bbb8f7f6b091e6d8de",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/wix2.2088c8d6.png"
+  },
+  {
+    "revision": "f5a7fb3c66e577fe7b2ba68c8de287eb",
+    "url": "%3Cusername%3E.github.io/%3Cgit-repo-name%3E/static/media/wix3.f5a7fb3c.png"
+  }
+]);
